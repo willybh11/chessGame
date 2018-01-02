1072ba31e113c5bb578e9b2fc60a520c8247988d
leo <leokastenberg@gmail.com>
2018-01-01 21:36:35 -0500

    continue work on JavaGraphics.java

--- gameFiles/JavaGraphics.java
+++ gameFiles/JavaGraphics.java
@@ -15,34 +15,46 @@ import javax.imageio.ImageIO;
 public class JavaGraphics extends JFrame
 {
 
-   public void paint(Graphics g)
-   {
-      try {
-        int row,col,x,y;
-        BufferedImage pawnImage;
-        pawnImage = ImageIO.read(getClass().getResourceAsStream("/pawnTest.jpg"));
+  public void paint(Graphics g)
+  {
+    try{
 
-        for ( row = 0;  row < 9;  row++ )
-        {
-           for ( col = 0;  col < 8;  col++)
-           {
-              x = col * 100;
-              y = row * 100;
-              if ((row % 2) == (col % 2)) {
-                 g.setColor(Color.WHITE);
-              } else {
-                 g.setColor(Color.BLACK);
-              }
+      //first I have to get the grid from the file
+      BufferedReader inputGrid = new BufferedReader(new FileReader("gridString.txt"));
+      String gridStringLine;
+      String gridString = "";
+      while ((gridStringLine = inputGrid.readLine()) != null){
+        gridString = gridString.concat(gridStringLine + "\n");
+      }
 
-              g.fillRect(x, y, 100, 100);
-              g.drawImage(pawnImage,x,y,50,50,null);
+      String[][] grid = new String[8][8];
 
-           }
-        }
-      } catch(IOException e) { e.printStackTrace(); }
-   }
+      int row,col,x,y;
+      BufferedImage pawnImage;
+      pawnImage = ImageIO.read(getClass().getResourceAsStream("/pawnTest.jpg"));
+
+      for ( row = 0;  row < 9;  row++ )
+      {
+         for ( col = 0;  col < 8;  col++)
+         {
+            x = col * 100;
+            y = row * 100;
+            if ((row % 2) == (col % 2)) {
+               g.setColor(Color.WHITE);
+            } else {
+               g.setColor(Color.BLACK);
+            }
+
+            g.fillRect(x, y, 100, 100);
+            g.drawImage(pawnImage,x,y,50,50,null);
+
+         }
+      }
+    } catch(IOException e) { e.printStackTrace(); }
+  }
 
    public static void main(String[] args) {
+
         JavaGraphics board = new JavaGraphics();
         board.setTitle("ChessBoard");
         board.setSize(800, 800);
